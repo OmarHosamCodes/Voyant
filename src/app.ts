3b1502919c8c0021835ad26a624cbf35ebf828f2
omarhosamcodes <omarhosamcodes@gmail.com>
2025-01-12 11:51:21 +0200

    Going live

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api", routes);
 
-const ONE_DAY_IN_MS = 24 * 60 * 60 * 1000;
-setInterval(runAnalysis, ONE_DAY_IN_MS);
+const RETRY_INTERVAL = process.env.RETRY_INTERVAL || "60000";
+setInterval(runAnalysis, Number.parseInt(RETRY_INTERVAL));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
